f9aeb7b5df1c42596a9673bb1253706b452c8461
Rye-Guy <ryanracioppo95@gmail.com>
2018-06-07 17:07:33 -0400

    html barebones

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB1_Gj7PzxD2l-rGa4HY80-A74Gnv4BXLI",
+    authDomain: "train-sechdule.firebaseapp.com",
+    databaseURL: "https://train-sechdule.firebaseio.com",
+    projectId: "train-sechdule",
+    storageBucket: "",
+    messagingSenderId: "305200297497"
+  };
+  firebase.initializeApp(config);
+
+  
